61bb6f208fc8770dbd3e9ba1c49126ab85fe7747
Topser99 <87140907+Paksan11@users.noreply.github.com>
2025-01-31 06:17:18 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -12,6 +12,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=a03s \
     PRODUCT_NAME=a03snnxx \
     PRIVATE_BUILD_DESC="a03s-user 13 TP1A.220624.014 A037FXXS7CXK1 release-keys"
+
 BUILD_FINGERPRINT := samsung/a03snnxx/a03s:13/TP1A.220624.014/A037FXXS7CXK1:user/test-keys
 PRODUCT_SHIPPING_API_LEVEL := 32
 
@@ -48,5 +49,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.crypto.volume.filenames_mode=aes-256-cts \
     ro.crypto.volume.metadata.method=dm-default-key \
     ro.crypto.dm_default_key.options_format.version=2 \
+    ro.crypto.metadata_init_delete_all_keys.enabled=false
     ro.crypto.volume.options=::v2 \
     keymaster_ver=4.0
+
+ADDITIONAL_SYSTEM_PROPERTIES += \
+ro.treble.enabled=true \
+ro.actionable_compatible_property.enabled=true \
+ro.postinstall.fstab.prefix=/system
+
+PRODUCT_SYSTEM_PROPERTIES += \
+ro.telephony.iwlan_operation_mode=legacy \
+debug.atrace.tags.enableflags=0 \
+persist.traced.enable=1 \
+external_storage.cross_user.enabled=1 \
+persist.sys.usb.config=mtp
