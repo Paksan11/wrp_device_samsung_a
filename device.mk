8c4b2e6550041273bf0bdb1ef4205d04f1efdf35
Topser99 <87140907+Paksan11@users.noreply.github.com>
2025-01-30 09:55:05 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -25,8 +25,8 @@ PRODUCT_PACKAGES += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-mtkimpl \
-    android.hardware.boot@1.2-mtkimpl.recovery
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery
 
 # Health Hal
 PRODUCT_PACKAGES += \
